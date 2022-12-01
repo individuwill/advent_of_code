3413dec05a4e35a132e71d9d52b73a1b9e2c3ece
Will Smith <Will.Smith@recursionpharma.com>
2022-12-01 10:29:37 -0800

    make code more functional

--- 2022/day_01/puzzle.py
+++ 2022/day_01/puzzle.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python
+import re
 from typing import List
 
 
-def sanitize_data(data: str) -> List[int]:
-    lines = [l.strip() for l in data.splitlines()]
-    return [0 if l == "" else int(l) for l in lines]
+def sanitize_data(data: str) -> List[List[str]]:
+    splits = [s.strip() for s in re.split(r"^$\n", data.strip(), flags=re.M)]
+    groups = [split.split("\n") for split in splits]
+    return groups
 
 
-def group_totals(lines: List[int]) -> List[int]:
-    total_calories = 0
-    all_totals = []
-    for line in lines:
-        if line == 0:
-            all_totals.append(total_calories)
-            total_calories = 0
-        else:
-            total_calories += int(line)
-    all_totals.append(total_calories)
-    return all_totals
+def get_int_groups(groups: List[List[str]]) -> List[List[int]]:
+    int_groups = [[int(g) for g in group] for group in groups]
+    return int_groups
+
+
+def get_sum_groups(int_groups: List[List[int]]) -> List[int]:
+    sum_groups = [sum(int_group) for int_group in int_groups]
+    return sum_groups
 
 
 def calculate_max(data: str) -> int:
-    lines = sanitize_data(data)
-    all_totals = group_totals(lines)
-    return max(all_totals)
+    groups = sanitize_data(data)
+    return max(get_sum_groups(get_int_groups(groups)))
 
 
 def calculate_top_3(data: str) -> int:
-    lines = sanitize_data(data)
-    all_totals = group_totals(lines)
-    all_totals.sort(reverse=True)
-    return sum(all_totals[0:3])
+    groups = sanitize_data(data)
+    return sum(sorted(get_sum_groups(get_int_groups(groups)), reverse=True)[0:3])
 
 
 def main():
