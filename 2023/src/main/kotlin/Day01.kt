1bfb83f65fa784ddbaaea1420704d166ac1dbf3f
Will Smith <wsmith85@gmail.com>
2023-12-01 12:33:13 -0800

    solution 2 working

--- 2023/src/main/kotlin/Day01.kt
+++ 2023/src/main/kotlin/Day01.kt
@@ -1,4 +1,87 @@
 class Day01 {
+
+    fun replaceAllNumbers(line: String): String {
+        return line.replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+    }
+    // "eight3one"
+    fun firstNumber(line: String): Int {
+        val leadingString = line.takeWhile { !it.isDigit() }
+        println("leadingString: $leadingString")
+        if(leadingString.isEmpty()) {
+            return line.first().digitToInt()
+        }
+        val firstDigit = if (leadingString.length == line.length) null else line[leadingString.length].digitToInt()
+        println("firstDigit: $firstDigit")
+        val leadingDigits = leadingString
+                .fold("") { acc, c ->
+                    replaceAllNumbers(acc + c)
+                }
+                .filter { it.isDigit() }
+                .map { it.digitToInt() }
+        return if(leadingDigits.isNotEmpty()) {
+            leadingDigits.first()
+        } else {
+            firstDigit!!
+        }
+    }
+
+    fun lastNumber(line: String): Int {
+        val lastDigitIndex = line.lastIndexOfAny("123456789".toCharArray())
+        val lastDigit = if(lastDigitIndex == -1) null else line[lastDigitIndex].digitToInt()
+        val trailingString = line.takeLast(line.length - lastDigitIndex - 1)
+        println("trailingString: $trailingString")
+        val numberizedTrailingString = trailingString.reversed().fold("") { acc, c ->
+            replaceAllNumbers(c + acc)
+        }
+        println("numberizedTrailingString: $numberizedTrailingString")
+        val trailingDigits = numberizedTrailingString
+                .filter { it.isDigit() }
+                .map { it.digitToInt() }
+        return if(trailingDigits.isNotEmpty()) {
+            trailingDigits.last()
+        } else {
+            lastDigit!!
+        }
+    }
+
+    fun numberize(line: String): String {
+        val numberized = line.fold("") { acc, c ->
+            // println("acc: $acc, c: $c")
+            (acc + c).replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+        }
+        val finalNumberized = numberized.replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+
+        if (finalNumberized != line) {
+            println("line: $line")
+            println("numberized: $numberized")
+            println("finalNumberized: $finalNumberized")
+        }
+        return finalNumberized
+    }
     fun decodeLine(line: String): Int {
         val justNumbers = line.filter(Char::isDigit)
         val desiredNumber = justNumbers.first().toString() + justNumbers.last().toString()
@@ -13,7 +96,15 @@ class Day01 {
     }
 
     fun solution02(input: String): Int {
-        return 0
+        val lines = input.split("\n")
+        val numbers = lines.map {
+            val firstNumber = firstNumber(it)
+            val lastNumber = lastNumber(it)
+            val combined = "$firstNumber$lastNumber"
+            Integer.parseInt(combined)
+        }
+        val total = numbers.sum()
+        return total
     }
 }
 
--- 2023/src/test/kotlin/Day01Test.kt
+++ 2023/src/test/kotlin/Day01Test.kt
@@ -1,5 +1,6 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class Day01Test {
 
@@ -18,8 +19,17 @@ class Day01Test {
 
     @Test
     fun testSolutionSample02() {
-        val expected = 1
-        val actual = today.solution02(sampleInput)
+        val expected = 281
+        val newSampleInput = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent()
+        val actual = today.solution02(newSampleInput)
         assertEquals(expected, actual)
     }
 
@@ -32,16 +42,66 @@ class Day01Test {
 
     @Test
     fun testSolution02() {
-        val expected = 1
+        val expected = 55686
         val actual = today.solution02(input)
+        print("actual: $actual")
+        assertTrue(actual > 55680)
         assertEquals(expected, actual)
     }
 
     @Test
-    fun testDecodeLine() {
+    fun testDecodeSimpleLine() {
         val input = "a1b2c3d4e5f"
         val expected = 15
         val actual = today.decodeLine(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNumberize() {
+        val input = "4nineeightseven2"
+        val expected = "49872"
+        val actual = today.numberize(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testOverlapping() {
+        val input = "eightwothree"
+        val expected = "8wo3"
+        val actual = today.numberize(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun firstNumber() {
+        val input = "two1eighthree"
+        val expected = 2
+        val actual = today.firstNumber(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun firstNumberAllWords() {
+        val input = "eightwothree"
+        val expected = 8
+        val actual = today.firstNumber(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lastNumber() {
+        val input = "two1eighthree"
+        val expected = 3
+        val actual = today.lastNumber(input)
+        assertEquals(expected, actual)
+    }
+
+        @Test
+    fun lastNumberAllWords() {
+        val input = "eightwothree"
+        val expected = 3
+        val actual = today.lastNumber(input)
         assertEquals(expected, actual)
     }
 }
